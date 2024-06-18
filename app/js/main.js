d7464c058cf0ac4060291204f051a2fd336a4c0e
Alexandra <alexandramarkova573@gmail.com>
2024-06-18 22:10:23 +0300

    переделала хедер html css,-  центрироваkf  логотип

--- app/js/main.js
+++ app/js/main.js
@@ -2,9 +2,11 @@
 'use strict';
 
 const burgerMenu = document.querySelector('.nav-icon');
+console.log(burgerMenu);
 const icons = burgerMenu.querySelectorAll('span');
 
-const menuList = document.querySelector('.menu__list');
+const menuList = document.querySelectorAll('.menu__list');
+console.log(menuList);
 
 if (icons) {
     burgerMenu.addEventListener('click', () => {
@@ -12,6 +14,9 @@ if (icons) {
         icons.forEach(icon => {
             icon.classList.toggle('_hidden');//меняю крестик на бургер
         });
-        menuList.classList.toggle('_active');
+        // menuList.classList.toggle('_active');
+        menuList.forEach(menu => {
+            menu.classList.toggle('_active');
+        })
     })
 }
